30acca4c20626a9811181f1a3a06f3879aa9c8e2
PauloIVM <paulo.vieira.marinho@gmail.com>
2024-08-17 09:27:23 -0300

    chore: some stuffs

--- packages/pack-a/src/index.ts
+++ packages/pack-a/src/index.ts
@@ -1,5 +1,11 @@
 import { version } from "../package.json";
 
+// Testar na raiz:
+// npm publish --access=public
+
+// INFO: Talvez seja melhor simplesmente usar o workspaces do yarn mesmo. Parece q tbm tem controle de versão.
+//       Estudar mais sobre...
+
 export default function getPackAVersion() {
     console.log("some chnage pack-a version: ", version);
     return version;
