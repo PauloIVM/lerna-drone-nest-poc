91f70899134ff14b773f3026e4303e77ec5cf8ba
PauloIVM <paulo.vieira.marinho@gmail.com>
2024-08-17 09:02:37 -0300

    chore: fake change

--- packages/pack-a/src/index.ts
+++ packages/pack-a/src/index.ts
@@ -1,6 +1,6 @@
 import { version } from "../package.json";
 
 export default function getPackAVersion() {
-    console.log("changed pack-a version: ", version);
+    console.log("pack-a version: ", version);
     return version;
 }
