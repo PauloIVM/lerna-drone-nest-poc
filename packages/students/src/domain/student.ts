1c57d476ef9127667d2b14cc448514dc324e3560
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-08-19 14:09:25 -0300

    chore: hot babel fix

--- packages/students/src/domain/student.ts
+++ packages/students/src/domain/student.ts
@@ -1,3 +1,13 @@
 export class Student {
-    constructor(readonly id: string, readonly name: string, readonly email: string) {}
+    id: string;
+    name: string;
+    email: string;
+    constructor(id: string, name: string, email: string) {
+        this.email = email;
+        this.name = name;
+        this.id = id;
+    }
+    // TODO: Esses modificadores deram problema com na hora de transpilar no babel. Se puder
+    //       fazer a build só com o tsc eu acho q é mais seguro...
+    // constructor(readonly id: string, readonly name: string, readonly email: string) {}
 }
